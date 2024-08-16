caf2b5c8017fab4a8eddc4b82ea7e1b9d650eea0
nox <noxhsxrk@icloud.com>
2024-08-17 02:00:11 +0700

    feat: Add script to create archive.zip excluding certain files/directories
    - Added "archive.zip" and updated ".gitignore" to exclude "archive.zip"
    - Included a new script "zip.sh" to zip current directory, excluding specific files and directories.

---
+++ scripts/zip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Define the name of the zip file
+ZIP_FILE="archive.zip"
+
+# Zip all files in the current directory, excluding specified files and directories
+zip -r $ZIP_FILE ./* -x "Makefile" "__pycache__/*" ".gitignore" "scripts/*" "README.md"
+
+echo "All files in the current directory have been zipped into $ZIP_FILE, excluding specified files."
