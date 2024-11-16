0920ee0221bff22865cedd16d70da10b08678bac
nox <noxhsxrk@icloud.com>
2024-11-17 00:22:03 +0700

    feat: refactor script

--- scripts/remove_dup_words.sh
+++ scripts/remove_dup_words.sh
@@ -5,8 +5,23 @@ file2="src/bot_commands/just_one/words.txt"
 
 remove_duplicates() {
     local file="$1"
-    sort "$file" | uniq >temp_file && mv temp_file "$file"
-    echo "Duplicates removed in the file: $file"
+    local temp_file="temp_file"
+    local original_sorted="original_sorted.txt"
+
+    sort "$file" >"$original_sorted"
+    sort "$file" | uniq >"$temp_file"
+
+    comm -23 "$original_sorted" "$temp_file" >removed_words.txt
+
+    if [ -s removed_words.txt ]; then
+        echo "Removed words:"
+        cat removed_words.txt
+    else
+        echo "No words were removed from the file: $file"
+    fi
+
+    mv "$temp_file" "$file"
+    rm "$original_sorted"
 }
 
 remove_duplicates "$file1"
